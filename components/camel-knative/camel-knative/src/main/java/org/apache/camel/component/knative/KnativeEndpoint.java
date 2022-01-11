bf4576d1feda028dd8da9d215246e09a7e850d77
Pasquale Congiusti <pasquale.congiusti@gmail.com>
2022-01-11 15:26:07 +0100

    Revert "fix(knative): use object name when events endpoints"
    This reverts commit cfa02982b8f50c1d92b028213676ab5a27020717.

--- components/camel-knative/camel-knative/src/main/java/org/apache/camel/component/knative/KnativeEndpoint.java
+++ components/camel-knative/camel-knative/src/main/java/org/apache/camel/component/knative/KnativeEndpoint.java
@@ -77,7 +77,7 @@ public class KnativeEndpoint extends DefaultEndpoint {
     }
 
     @Override
-    public Producer createProducer() {
+    public Producer createProducer() throws Exception {
         final KnativeResource service = lookupServiceDefinition(Knative.EndpointKind.sink);
         final Processor ceProcessor = cloudEventProcessor.producer(this, service);
         final Producer producer = getComponent().getProducerFactory().createProducer(this, createTransportConfiguration(service), service);
@@ -140,20 +140,21 @@ public class KnativeEndpoint extends DefaultEndpoint {
     }
 
     @Override
-    protected void doInit() {
+    protected void doInit() throws Exception {
         if (ObjectHelper.isEmpty(this.configuration.getTypeId())) {
             this.configuration.setTypeId(this.typeId);
         }
     }
 
     KnativeResource lookupServiceDefinition(Knative.EndpointKind endpointKind) {
-        final String resourceName;
-        if (type == Knative.Type.event && configuration.getName() != null) {
-            resourceName = configuration.getName();
-        } else {
-            resourceName = configuration.getTypeId();
-       }
+        final String resourceName = configuration.getTypeId();
 
+        //
+        // look-up service definition by service name first then if not found try to look it up by using
+        // "default" as a service name. For channels and endpoints, the service name can be derived from
+        // the endpoint uri but for events it is not possible so default should always be there for events
+        // unless the service name is define as an endpoint option.
+        //
         KnativeResource resource = lookupServiceDefinition(resourceName, endpointKind)
             .or(() -> lookupServiceDefinition("default", endpointKind))
             .orElseThrow(() -> new IllegalArgumentException(
@@ -198,9 +199,9 @@ public class KnativeEndpoint extends DefaultEndpoint {
         // For event type endpoints se need to add an additional filter to filter out events received
         // based on the given type.
         //
-        if (resource.getType() == Knative.Type.event && ObjectHelper.isNotEmpty(configuration.getTypeId())) {
-            answer.setCloudEventType(configuration.getTypeId());
-            answer.addFilter(CloudEvent.CAMEL_CLOUD_EVENT_TYPE, configuration.getTypeId());
+        if (resource.getType() == Knative.Type.event && ObjectHelper.isNotEmpty(resourceName)) {
+            answer.setCloudEventType(resourceName);
+            answer.addFilter(CloudEvent.CAMEL_CLOUD_EVENT_TYPE, resourceName);
         }
 
         return answer;
@@ -229,17 +230,24 @@ public class KnativeEndpoint extends DefaultEndpoint {
     }
 
     private static Predicate<KnativeResource> serviceFilter(KnativeConfiguration configuration, Knative.EndpointKind endpointKind) {
-        return resource -> {
-            if (!Objects.equals(endpointKind, resource.getEndpointKind())) {
-                return false;
+        return new Predicate<KnativeResource>() {
+            @Override
+            public boolean test(KnativeResource resource) {
+                if (!Objects.equals(endpointKind, resource.getEndpointKind())) {
+                    return false;
+                }
+                if (configuration.getApiVersion() != null && !Objects.equals(resource.getObjectApiVersion(), configuration.getApiVersion())) {
+                    return false;
+                }
+                if (configuration.getKind() != null && !Objects.equals(resource.getObjectKind(), configuration.getKind())) {
+                    return false;
+                }
+                if (configuration.getName() != null && !Objects.equals(resource.getObjectName(), configuration.getName())) {
+                    return false;
+                }
+
+                return true;
             }
-            if (configuration.getApiVersion() != null && !Objects.equals(resource.getObjectApiVersion(), configuration.getApiVersion())) {
-                return false;
-            }
-            if (configuration.getKind() != null && !Objects.equals(resource.getObjectKind(), configuration.getKind())) {
-                return false;
-            }
-            return configuration.getName() == null || Objects.equals(resource.getObjectName(), configuration.getName());
         };
     }
 }
